7756b323f0c52ec096221be0548126ecc7284ca6
amirisback <faisalamircs@gmail.com>
2019-04-15 13:37:04 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/frogoboxnews/model/News.kt
@@ -0,0 +1,19 @@
+package com.frogobox.frogoboxnews.model
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * FrogoBoxNews
+ * Copyright (C) 15/04/2019.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Line     : bullbee117
+ * Phone    : 081357108568
+ * Majors   : D3 Teknik Informatika 2016
+ * Campus   : Telkom University
+ * -----------------------------------------
+ * id.amirisback.frogobox
+ */
